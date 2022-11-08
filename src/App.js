2c78e292ef6a4c1314eabcc7ea8db00e802f19d9
rejaul-karim10 <rejalkarim66666@gmail.com>
2022-11-09 02:03:43 +0800

    added routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import { RouterProvider } from 'react-router-dom';
 import './App.css';
+import { router } from './Pages/Router/Routes/Routes';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World !!!</h1>
+    <div >
+      <RouterProvider router={router}></RouterProvider>
     </div>
   );
 }
---
+++ src/Pages/Router/Routes/Routes.jsx
@@ -0,0 +1,18 @@
+import { createBrowserRouter } from "react-router-dom";
+import Main from "../../../layout/Main";
+import Home from "../../Home/Home";
+
+export const router = createBrowserRouter([
+    {
+        path:'/',
+        element: <Main/>,
+        children:[
+            {
+                path:'/',
+                element: <Home/>
+            }
+        ]
+    }
+])
+
+
