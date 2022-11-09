91f527a5fcfd01cd154c5ec6f7445fdd8c868b75
rejaul-karim10 <rejalkarim66666@gmail.com>
2022-11-10 02:17:54 +0800

    added question and answer

--- src/Pages/Blog/Blog.jsx
+++ src/Pages/Blog/Blog.jsx
@@ -17,8 +17,7 @@ const Blog = () => {
             Difference between SQL and NoSQL
             </summary>
             <p className="px-4 py-6 pt-0 ml-4 -mt-4 dark:text-gray-400">
-              Lectus iaculis orci metus vitae ligula dictum per. Nisl per nullam
-              taciti at adipiscing est.{" "}
+            SQL databases are vertically scalable, while NoSQL databases are horizontally scalable. SQL databases are table-based, while NoSQL databases are document, key-value, graph, or wide-column stores. SQL databases are better for multi-row transactions, while NoSQL is better for unstructured data like documents or JSON.{" "}
             </p>
           </details>
           <details className="w-full border rounded-lg">
@@ -26,8 +25,7 @@ const Blog = () => {
             What is JWT, and how does it work?
             </summary>
             <p className="px-4 py-6 pt-0 ml-4 -mt-4 dark:text-gray-400">
-              Tincidunt ut hac condimentum rhoncus phasellus nostra. Magna
-              porttitor egestas tincidunt neque vehicula potenti.{" "}
+            JSON Web Token (JWT) is an open standard (RFC 7519) that defines a compact and self-contained way for securely transmitting information between parties as a JSON object. This information can be verified and trusted because it is digitally signed.{" "}
             </p>
           </details>
           <details className="w-full border rounded-lg">
@@ -35,9 +33,7 @@ const Blog = () => {
             What is the difference between javascript and NodeJS?
             </summary>
             <p className="px-4 py-6 pt-0 ml-4 -mt-4 dark:text-gray-400">
-              Justo libero tellus integer tincidunt justo semper consequat
-              venenatis aliquet imperdiet. Ultricies urna proin fusce nulla
-              pretium sodales vel magna et massa euismod vulputate sed.{" "}
+            JavaScript is a simple programming language that can be used with any browser that has the JavaScript Engine installed. Node. js, on the other hand, is an interpreter or execution environment for the JavaScript programming language.{" "}
             </p>
           </details>
           <details className="w-full border rounded-lg">
@@ -45,9 +41,7 @@ const Blog = () => {
             How does NodeJS handle multiple requests at the same time?
             </summary>
             <p className="px-4 py-6 pt-0 ml-4 -mt-4 dark:text-gray-400">
-              Justo libero tellus integer tincidunt justo semper consequat
-              venenatis aliquet imperdiet. Ultricies urna proin fusce nulla
-              pretium sodales vel magna et massa euismod vulputate sed.{" "}
+            How NodeJS handle multiple client requests? NodeJS receives multiple client requests and places them into EventQueue. NodeJS is built with the concept of event-driven architecture. NodeJS has its own EventLoop which is an infinite loop that receives requests and processes them.{" "}
             </p>
           </details>
         </div>
--- src/contacts/AuthProvider/AuthProvider.jsx
+++ src/contacts/AuthProvider/AuthProvider.jsx
@@ -1,5 +1,5 @@
 import React, { createContext, useEffect, useState } from "react";
-import { createUserWithEmailAndPassword, getAuth, onAuthStateChanged } from "firebase/auth";
+import { createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword } from "firebase/auth";
 import app from "../../firebase/firebase.config";
 
 export const AuthContext = createContext();
@@ -13,6 +13,11 @@ const AuthProvider = ({children}) => {
     return createUserWithEmailAndPassword(auth, email, password)
   }
 
+  const login =(email, password)=>{
+    return signInWithEmailAndPassword(auth, email, password)
+
+  }
+
   useEffect(()=>{
     const unsubscribe = onAuthStateChanged(auth, currentUser=>{
         console.log(currentUser)
@@ -24,7 +29,7 @@ const AuthProvider = ({children}) => {
     }
   },[])
 
-  const authInfo = {user, loading, createUser};
+  const authInfo = {user, loading, createUser, login};
   return <AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>;
 };
 
